209ae811d8f77b3275355752787d7f7bf2ad4d9f
Utpal Singh <utpalsingh915@gmail.com>
2023-10-26 10:37:26 +0530

    login image added

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,18 +1,26 @@
 "use client";
 import React from "react";
 import { signIn } from "next-auth/react";
+import Image from "next/image";
 const page = () => {
   return (
     //starts
-    <div className="w-full flex items-center justify-center h-screen">
+    <div className="w-full flex items-center flex-col gap-16 justify-center h-screen">
+      <Image
+        alt="login-img"
+        src="/login.png"
+        width={150}
+        className="w-64 h-auto rounded-lg"
+        height={150}
+      />
       <button
         onClick={() => signIn("google")}
-        className="px-3 py-3 bg-blue-800 rounded-lg text-white hover:bg-blue-900"
+        className="px-3 py-3 mx-4 bg-blue-800 rounded-lg text-white hover:bg-blue-900"
       >
         Continue with Google
       </button>
     </div>
-  );//end
+  ); //end
 };
 
 export default page;
